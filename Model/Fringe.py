58a3f2ff1c31b39e7cc7dde1ca0f84a474304d36
Omar khairat <omarkhairat133@gmail.com>
2022-10-24 11:14:23 +0200

    finally

--- Model/Fringe.py
+++ Model/Fringe.py
@@ -1,4 +1,6 @@
 import heapq
+from queue import PriorityQueue
+from collections import defaultdict
 from abc import ABC, abstractmethod
 import sys
 from typing import List, Tuple
@@ -54,6 +56,7 @@ class DFSFringe(Fringe):
     def get_max_size(self):
         return self.__max_size
 
+
 class BFSFringe(Fringe):
 
     def __init__(self):
@@ -80,22 +83,43 @@ class BFSFringe(Fringe):
 class ManhattanFringe(Fringe):
 
     def __init__(self):
-        self.__pq: List[Tuple[int, int, PuzzleBoard]] = []
+        self.__pq: PriorityQueue[Tuple[int, int, PuzzleBoard]] = PriorityQueue()
+        # self.__sdic = defaultdict(lambda: 999999)
         self.__max_size = 0
         self.__buff = 0
 
     def get_node(self) -> PuzzleBoard:
-        _, _, ans = heapq.heappop(self.__pq)
+        # mmm = 999999
+        # for x in self.__pq:
+        #     print("\tff :", x[0])
+        #     mmm = min(mmm, x[0])
+
+        _, _, ans = self.__pq.get()
+        # if cost != self.__sdic[tuple(ans.get_state())]:
+        #     ans = self.get_node()
+        # print("\t\t\t\t\tselected :", pp)
+        # print("\t\t\t\t\tMin :", mmm)
+        # print("\n\n\n\n\n******************************")
         return ans
 
     def add_node(self, val: PuzzleBoard, services: BoardServices = None) -> None:
+        cost = services.manhattan_h(val.get_state()) + val.get_depth()
+        # print(self.__sdic[tuple(val.get_state())])
+        # if cost >= self.__sdic[tuple(val.get_state())]:
+        #     print("pass")
+        #     return
         self.__buff += 1
-        heapq.heappush(self.__pq, (services.manhattan_h(val.get_state()) + 1, self.__buff, val))
+        # self.__sdic[tuple(val.get_state())] = cost
+        self.__pq.put((services.manhattan_h(val.get_state()) + val.get_depth(), self.__buff, val))
+        # heapq.heappush(self.__pq, (services.manhattan_h(val.get_state()) + val.get_depth(), self.__buff, val))
+        # for xx in self.__pq:
+        #     print(xx[0])
+        # print("\n\n\n\n\n******************************")
         # print(f"Man: {services.manhattan_h(val.get_state()) + 1}")
         self.__max_size = max(self.get_size(), self.__max_size)
 
     def get_size(self) -> int:
-        return len(self.__pq)
+        return self.__pq.qsize()
 
     def is_empty(self) -> bool:
         return self.get_size() == 0
@@ -107,22 +131,22 @@ class ManhattanFringe(Fringe):
 class EuclideanFringe(Fringe):
 
     def __init__(self):
-        self.__pq: List[Tuple[float, int, PuzzleBoard]] = []
+        self.__pq: PriorityQueue[Tuple[float, int, PuzzleBoard]] = PriorityQueue()
         self.__max_size = 0
         self.__buff = 0
 
     def get_node(self) -> PuzzleBoard:
-        _, _, ans = heapq.heappop(self.__pq)
+        _, _, ans = self.__pq.get()
         return ans
 
     def add_node(self, val: PuzzleBoard, services: BoardServices = None) -> None:
         self.__buff += 1
-        heapq.heappush(self.__pq, (services.euclidean_h(val.get_state()) + 1.0, self.__buff, val))
+        self.__pq.put((services.euclidean_h(val.get_state()) + float(val.get_depth()), self.__buff, val))
         # print(f"euc: {services.euclidean_h(val.get_state()) + 1}")
         self.__max_size = max(self.get_size(), self.__max_size)
 
     def get_size(self) -> int:
-        return len(self.__pq)
+        return self.__pq.qsize()
 
     def is_empty(self) -> bool:
         return self.get_size() == 0
--- Services/SearchStrategy.py
+++ Services/SearchStrategy.py
@@ -42,6 +42,8 @@ class SearchStrategy(ABC):
         return ans
 
 
+
+
 class DFSStrategy(SearchStrategy):
 
     def search(self, initial_puzzle: PuzzleBoard, services: BoardServices) -> Answer:
@@ -57,10 +59,85 @@ class BFSStrategy(SearchStrategy):
 class AstarManhattanStrategy(SearchStrategy):
 
     def search(self, initial_puzzle: PuzzleBoard, services: BoardServices) -> Answer:
-        return super()._search_fringe(initial_puzzle, services, "a*manhattan")
+        nodes_expanded = 0
+        max_depth = 0
+        found = False
+        explored_states = set([])
+        fringe_dict = {}
+        fringe_factory = FringeFactory()
+        fringe = fringe_factory.create_fringe("a*manhattan")
+        fringe.add_node(initial_puzzle, services)
+        # fringe_set.add(tuple(initial_puzzle.get_state()))
+        fringe_dict[tuple(initial_puzzle.get_state())] = 0
+        ans_board = None
+        ans = Answer()
+        while not fringe.is_empty():
+            cur_board: PuzzleBoard = fringe.get_node()
+            # print("gg/t",fringe.get_size())
+            if tuple(cur_board.get_state()) in explored_states:
+                # print("continue")
+                continue
+            # if tuple(cur_board.get_state()) in fringe_set:
+            #     # print("wefwf")
+            #     fringe_set.remove(tuple(cur_board.get_state()))
+            fringe_dict.pop(tuple(cur_board.get_state()))
+            nodes_expanded += 1
+            explored_states.add(tuple(cur_board.get_state()))
+            if services.is_goal(cur_board.get_state()):
+                found = True
+                ans_board = cur_board
+                break
+            for child in cur_board.get_children():
+                if tuple(child.get_state()) not in explored_states and tuple(
+                        child.get_state()) not in fringe_dict.keys():
+                    fringe.add_node(child, services)
+                    fringe_dict[tuple(child.get_state())] = child.get_depth() + services.manhattan_h(
+                        child.get_state())
+                    max_depth = max(max_depth, child.get_depth())
+                elif tuple(child.get_state()) not in explored_states:
+                    temp_cost = child.get_depth() + services.manhattan_h(child.get_state())
+                    if temp_cost < fringe_dict[tuple(child.get_state())]:
+                        fringe.add_node(child, services)
+                        fringe_dict[tuple(child.get_state())] = temp_cost
+        ans.add_answer_attr(ans_board, found, max_depth, nodes_expanded, fringe.get_size(), fringe.get_max_size())
+        return ans
 
 
 class AstarEuclidStrategy(SearchStrategy):
 
     def search(self, initial_puzzle: PuzzleBoard, services: BoardServices) -> Answer:
-            return super()._search_fringe(initial_puzzle, services, "a*euclidean")
+        nodes_expanded = 0
+        max_depth = 0
+        found = False
+        explored_states = set([])
+        fringe_dict = {}
+        fringe_factory = FringeFactory()
+        fringe = fringe_factory.create_fringe("a*euclidean")
+        fringe.add_node(initial_puzzle, services)
+        fringe_dict[tuple(initial_puzzle.get_state())] = 0
+        ans_board = None
+        ans = Answer()
+        while not fringe.is_empty():
+            cur_board: PuzzleBoard = fringe.get_node()
+            if tuple(cur_board.get_state()) in explored_states:
+                continue
+            fringe_dict.pop(tuple(cur_board.get_state()))
+            nodes_expanded += 1
+            explored_states.add(tuple(cur_board.get_state()))
+            if services.is_goal(cur_board.get_state()):
+                found = True
+                ans_board = cur_board
+                break
+            for child in cur_board.get_children():
+                if tuple(child.get_state()) not in explored_states and tuple(
+                        child.get_state()) not in fringe_dict.keys():
+                    fringe.add_node(child, services)
+                    fringe_dict[tuple(child.get_state())] = child.get_depth() + services.euclidean_h(child.get_state())
+                    max_depth = max(max_depth, child.get_depth())
+                elif tuple(child.get_state()) not in explored_states:
+                    temp_cost = child.get_depth() + services.euclidean_h(child.get_state())
+                    if temp_cost < fringe_dict[tuple(child.get_state())]:
+                        fringe.add_node(child, services)
+                        fringe_dict[tuple(child.get_state())] = temp_cost
+        ans.add_answer_attr(ans_board, found, max_depth, nodes_expanded, fringe.get_size(), fringe.get_max_size())
+        return ans
