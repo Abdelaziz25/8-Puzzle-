0d2808d7b2307d9a69366c480d8b242650eccf4e
Mento79 <bodyomar79@gmail.com>
2022-10-27 02:15:17 +0200

    adding Check for winning

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from tkinter import *
 
 arr=[[0,6,2],[3,5,8],[4,6,7]]
-arr_squares=[[],[],[],[],[],[]]
 arr_circles=[[],[],[],[],[],[]]
-arr_pieces=[]
+board=[[0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0],
+       [0,0,0,0,0,0,0],
+       ]
 arr_numbers=[[],[],[]]
 red="#a42b39"
 background_color="black"
@@ -45,32 +50,12 @@ def tab1():
           buttonmove.destroy()
           tab1()
 
-      def move(x_new,y_new ,x_old,y_old):
-          x_difference=y_new-y_old
-          y_difference=x_new-x_old
-          for i in range(moving_period):
-              mycanvas.move(arr_squares[x_old][y_old], velocity * x_difference, velocity * y_difference)
-              mycanvas.move(arr_numbers[x_old][y_old], velocity * x_difference, velocity * y_difference)
-              mycanvas.update()
-          arr_squares[x_new][y_new] = arr_squares[x_old][y_old]
-          arr_numbers[x_new][y_new] = arr_numbers[x_old][y_old]
-          arr_squares[x_old][y_old] = 0
-          arr_numbers[x_old][y_old] = 0
-
-      def next():
-          move( moves_arr[counter - 1][0] , moves_arr[counter - 1][1],
-                moves_arr[counter][0] , moves_arr[counter][1])
-          counter += 1
-
-      def previous():
-          move( moves_arr[counter][0] , moves_arr[counter][1],
-                moves_arr[counter - 1][0] , moves_arr[counter - 1][1])
-          counter -= 1
 
       def auto():
           while counter < len(moves_arr):
               next()
       def draw (i, j ):
+          #print (i , j, 5-col[i] )
           global color
           x = i * (2 + square_length)
           starx = i * (2 + square_length) + (20)
@@ -83,6 +68,7 @@ def tab1():
                                       stary + (49.5 * scale), starx + (47.5 * scale), stary + (19.5 * scale),
                                       starx + (2.5 * scale), stary + (19.5 * scale), starx + (40 * scale),
                                       stary + (49.5 * scale), fill="", outline=red, width="2")
+              board[5 - col[i]][i]=1
           else:
               mycanvas.create_oval(x + 1, y + 1, x + (square_length) - 2, y + (square_length) - 2, fill="#e3c559",width="0")
               mycanvas.create_oval(x + circle_scale, y + circle_scale, x + (square_length) - circle_scale - 1,y + (square_length) - circle_scale - 1, fill="#c1bc2f", outline="black",width="0", )
@@ -90,17 +76,42 @@ def tab1():
                                       stary + (49.5 * scale), starx + (47.5 * scale), stary + (19.5 * scale),
                                       starx + (2.5 * scale), stary + (19.5 * scale), starx + (40 * scale),
                                       stary + (49.5 * scale), fill="", outline="#e3c559", width="2")
+              board[5 - col[i]][i] = 2
+          #hntcheck hina
+          check(5 - col[i], i)
           color = not color
           col[i]+=1
-      y1=0
+          #print(board)
+
+      def check(x,y):
+          piece= 1 if color else 2
+          #down
+          if(x<=2):
+              if(board[x+1][y]==piece and board[x+2][y]==piece and board[x+3][y]==piece):
+                  print( "kisbnaaaaaa ta7t")
+          #right and left
+          for i in range(4):
+              if (board[x][i] == piece and board[x][i+1] == piece and board[x][i+2] == piece and board[x][i+3] == piece):
+                  print("kisbnaaaaaa 3la nafs el5t")
+          #digonally
+          for i in range(4):
+              for j in range (3):
+                  if (board[j][i] == piece and board[j+1][i+1] == piece and board[j+2][i+2] == piece and board[j+3][i+3] == piece):
+                      print("kisbnaaaaaa diagonal tal3 4imal")
+          #reverse diagonal
+          for i in range(4):
+              for j in range (5,2,-1):
+                  if (board[j][i] == piece and board[j-1][i+1] == piece and board[j-2][i+2] == piece and board[j-3][i+3] == piece):
+                      print("kisbnaaaaaa diagonal tale3 ymin")
+
       for i in range (0,6):
           y = i * (2+square_length)
           for j in range(0,7):
               x=j * (2+square_length)
-              arr_squares[i].append(mycanvas.create_rectangle(x , y , x + (square_length) , y +(square_length) , outline=foreground_color, fill=foreground_color ,width = "2"))
+              mycanvas.create_rectangle(x , y , x + (square_length) , y +(square_length) , outline=foreground_color, fill=foreground_color ,width = "2")
               arr_circles[i].append(mycanvas.create_oval(x +1 , y +1 , x + (square_length) -2, y +(square_length) -2 ,fill="black" ,width = "0"))
               mycanvas.tag_bind(arr_circles[i][j],"<Button-1>",lambda x: draw(x.x//100,x.y//100))
-      #mycanvas.move(arr_pieces[0], )
+
       button1.destroy()
       button2.destroy()
       button3.destroy()
